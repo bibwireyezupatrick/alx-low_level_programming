5c3d761a00ef93c23382976c0258faaeb7dda566
bibwireyezupatrick <bibwireyezupatrick@gmail.com>
2023-08-24 16:39:58 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,7 +14,7 @@ n1 = n;
 
 if (n < 0)
 {
-putchar('-');
+_putchar('-');
 n1 = -n;
 }
 
@@ -22,5 +22,5 @@ if (n1 / 10 != 0)
 {
 print_number(n1 / 10);
 }
-putchar((n1 % 10) + '0');
+_putchar((n1 % 10) + '0');
 }
