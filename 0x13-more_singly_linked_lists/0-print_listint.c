933f91e96e2fa52805d6e9a89ee94b59e0215db5
bibwireyezupatrick <bibwireyezupatrick@gmail.com>
2023-09-26 08:08:16 -0700

    print elements of list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,20 +3,20 @@
 /**
  * print_listint-prints all elements of the list
  * @h-a linked list
- *
+ *node_count-number of nodes
  * Return-number of nodes
  */
 
 size_t print_listint(const listint_t *h);
 {
-	size_t node_count=0;
+	size_t node_count = 0;
 
-	while(h!=NULL)
+	while (h != NULL)
 	{
-		printf("%d\n",h->n);
-		h=h->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		node_count++;
 	}
 
-	return node_count;
+	return (node_count);
 }
