1c183b8a7f8fb34022fc10dec19cac93564eb8f0
bibwireyezupatrick <bibwireyezupatrick@gmail.com>
2023-09-26 08:58:31 -0700

    print length of the list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len-prints the number of elements of the list
+ * @h: a pointer to the linked list
+ *
+ * Return: number of elements in list
+ */
+
+size_t listint_len(const listint_t *h);
+{
+	size_t len = 0;
+
+	while (h != 0)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (len);
+}
+
+
