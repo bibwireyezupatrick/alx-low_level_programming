a53218842e8c0d360f13e6c653beac15e98798a3
bibwireyezupatrick <bibwireyezupatrick@gmail.com>
2023-09-26 08:17:08 -0700

    corrected

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_listint-prints all elements of the list
- * @h-a linked list
- *node_count-number of nodes
- * Return-number of nodes
+ * print_listint - prints all elements of the list
+ * @h: a linked list
+ *node: count-number of nodes
+ * Return: number of nodes
  */
 
 size_t print_listint(const listint_t *h);
